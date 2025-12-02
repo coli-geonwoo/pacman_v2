4fda14cf5d5def0c671e576f1e6722d5c26f5059
coli <kkwoo001021@naver.com>
2025-12-02 17:56:30 +0900

    chore: 무의미한 주석 제거

--- src/main/java/game/GameLauncher.java
+++ src/main/java/game/GameLauncher.java
@@ -3,7 +3,6 @@ package game;
 import javax.swing.*;
 import java.io.IOException;
 
-//Point d'entrée de l'application
 //애플리케이션 진입점
 public class GameLauncher {
     private static UIPanel uiPanel;
@@ -15,7 +14,6 @@ public class GameLauncher {
 
         JPanel gameWindow = new JPanel();
 
-        //Création de la "zone de jeu"
         //"플레이 영역" 생성
         try {
             gameWindow.add(new GameplayPanel(448,496));
@@ -23,7 +21,6 @@ public class GameLauncher {
             e.printStackTrace();
         }
 
-        //Création de l'UI (pour afficher le score)
         //UI 생성(점수 표시용)
         uiPanel = new UIPanel(256,496);
         gameWindow.add(uiPanel);
